623cd6a765d1fe62ed45f8947c762f7eda8673c2
Matej <ruzicka.maty@gmail.com>
2025-01-26 14:35:06 +0100

    backported to 1.20.4

---
+++ common/src/main/java/com/mars/blastingsand/CommonClass.java
@@ -0,0 +1,17 @@
+package com.mars.blastingsand;
+
+import com.mars.deimos.config.DeimosConfig;
+import com.mars.deimos.datagen.DeimosRecipeGenerator;
+import net.minecraft.resources.ResourceLocation;
+
+import static com.mars.blastingsand.Constants.MOD_ID;
+
+public class CommonClass {
+    public static void init() {
+        DeimosConfig.init(MOD_ID, BlastingSandConfig.class);
+
+        for (String sand : BlastingSandConfig.sand_list) {
+            DeimosRecipeGenerator.createBlastingJson(new ResourceLocation(sand), new ResourceLocation("minecraft:glass"), 100, 0.1F);
+        }
+    }
+}
---
+++ common/src/main/java/com/mars/blastingsand/Constants.java
@@ -0,0 +1,11 @@
+package com.mars.blastingsand;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "blastingsand";
+    public static final String MOD_NAME = "Smelting Sand in a Blast Furnace";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
